56086da21cfb1d2ef29597a55c56d6965d90feb5
Litvinov Yuriy <u.litvinov@elewise.com>
2020-06-08 01:34:46 +0400

    init(main): finished 1600

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
-    
+    $('#phone').mask('+38 (096) 777 77 77');
+    $('.about-us__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        dots: false
+    });
 });
